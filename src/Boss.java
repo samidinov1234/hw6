8254abd596d043ba15dd227d7a6fced795bda8b0
samidinov1234 <eldosabdykdayrov75@gmail.com>
2023-12-20 20:27:01 +0600

    Initial commit

---
+++ src/Boss.java
@@ -0,0 +1,16 @@
+public class Boss extends GameEntity{
+    Weapon weapon = new Weapon();
+
+
+    public Boss(int hp, int damege, String name) {
+        super(hp, damege, name);
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+}
